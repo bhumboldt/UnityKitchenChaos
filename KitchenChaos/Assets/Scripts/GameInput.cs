899ce7053fe4125190846c9dfb9839a0a3e658d8
Brandon Humboldt <brhumboldt@gmail.com>
2024-12-13 14:02:26 -0600

    Input system refactor to use new input system

---
+++ KitchenChaos/Assets/Scripts/GameInput.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+public class GameInput : MonoBehaviour
+{
+    private PlayerInputActions playerInputActions;
+    
+    private void Awake()
+    {
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+    }
+
+    public Vector2 GetMovementVectorNormalized()
+    {
+        Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
+        
+        // Normalize so we have the same speed no matter what is pressed
+        inputVector = inputVector.normalized;
+
+        return inputVector;
+    }
+}
--- KitchenChaos/Assets/Scripts/Player.cs
+++ KitchenChaos/Assets/Scripts/Player.cs
@@ -4,33 +4,13 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private GameInput gameInput;
 
     private bool isWalking;
     
     private void Update()
     {
-        // Separating this out because for input, we only have two axes
-        Vector2 inputVector = new Vector2(0, 0);
-        
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = 1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = 1;
-        }
-        
-        // Normalize so we have the same speed no matter what is pressed
-        inputVector = inputVector.normalized;
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
         isWalking = moveDir != Vector3.zero;
