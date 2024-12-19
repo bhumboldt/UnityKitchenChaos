c366018ed124d90863c0b1b735cb98a31154f888
Brandon Humboldt <brhumboldt@gmail.com>
2024-12-18 18:57:02 -0600

    player collision

--- KitchenChaos/Assets/Scripts/Player.cs
+++ KitchenChaos/Assets/Scripts/Player.cs
@@ -13,11 +13,43 @@ public class Player : MonoBehaviour
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+
+        float playerRadius = 0.7f;
+        float playerHeight = 2f;
+        float moveDistance = Time.deltaTime * moveSpeed;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+        if (!canMove)
+        {
+            // Cannot move towards this direction, split and move either just x or z
+            // Attempt only x movement
+            Vector3 moveDirx = new Vector3(moveDir.x, 0f, 0f).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirx, moveDistance);
+
+            if (canMove)
+            {
+                // Can Move only on the x axis
+                moveDir = moveDirx;
+            }
+            else
+            {
+                // Attempt z movement
+                Vector3 moveDirZ = new Vector3(0f, 0f, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+
+                if (canMove)
+                {
+                    moveDir = moveDirZ;
+                }
+            }
+        }
+        
+        if (canMove)
+        {
+            transform.position += moveDir * moveDistance;
+        }
+        
         isWalking = moveDir != Vector3.zero;
-        
-        float speed = Time.deltaTime * moveSpeed;
-        
-        transform.position += moveDir * speed;
         
         // Rotate the character to face walking direction. Use slerp for rotations
         float rotateSpeed = 10f;
